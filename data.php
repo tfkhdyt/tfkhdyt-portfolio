c8f4e70bd7f8aaadf09f26018ee34cfa1fa1cede
tfkhdyt <ta142fa@gmail.com>
2021-02-19 13:29:53 +0700

    initial release

---
+++ data.php
@@ -0,0 +1,67 @@
+<?php
+  $data[0]['nama'] = "Ultra Video Compressor";
+  $data[0]['deskripsi'] = "A script to compress video size without losing video quality on Termux";
+  $data[0]['tipe'] = "app";
+  $data[0]['link'] = "https://github.com/tfkhdyt/uvc";
+  $data[0]['foto'] = "assets/img/portfolio/portfolio-1.jpg";
+
+  $data[1]['nama'] = "Astrophotography";
+  $data[1]['deskripsi'] = "26 Agustus 2020";
+  $data[1]['tipe'] = "art";
+  $data[1]['link'] = "https://www.instagram.com/p/CEVYVw_gi3-/?utm_source=ig_web_copy_link";
+  $data[1]['foto'] = "https://scontent-frt3-1.cdninstagram.com/v/t51.2885-15/e35/118296687_3209493245773161_2670363808048102388_n.jpg?_nc_ht=scontent-frt3-1.cdninstagram.com&_nc_cat=109&_nc_ohc=2nH02YDHPvQAX_PN1d5&tp=1&oh=65538573959190aa4d2a86e5b058c56f&oe=6054EC44&dl=1";
+ 
+  $data[2]['nama'] = "Spotify Downloader";
+  $data[2]['deskripsi'] = "A script to download Spotify music into mp3 easily";
+  $data[2]['tipe'] = "app";
+  $data[2]['link'] = "https://github.com/tfkhdyt/SpotifyDL";
+  $data[2]['foto'] = "assets/img/portfolio/portfolio-2.jpg";
+ 
+  $data[3]['nama'] = "Formula E SRT05E";
+  $data[3]['deskripsi'] = "1 Desember 2019";
+  $data[3]['tipe'] = "art";
+  $data[3]['link'] = "https://www.instagram.com/p/B5hxECdAXdQ/?igshid=9oicfesy8wfk";
+  $data[3]['foto'] = "https://instagram.fbdo1-2.fna.fbcdn.net/v/t51.2885-15/e35/74705000_179257256589868_2821042374306544775_n.jpg?_nc_ht=instagram.fbdo1-2.fna.fbcdn.net&_nc_cat=104&_nc_ohc=jrIczBofd_EAX_M0Xw5&tp=1&oh=d72e4ccf8e7e59768c1ae66afe3a4e75&oe=6056C914";
+  
+  $data[4]['nama'] = "Wangy Wangy Generator";
+  $data[4]['deskripsi'] = "Skrip untuk membuat copypasta wangy wangy dengan Termux";
+  $data[4]['tipe'] = "app";
+  $data[4]['link'] = "https://github.com/tfkhdyt/wangy";
+  $data[4]['foto'] = "assets/img/portfolio/wangy.jpg";
+  
+  $data[5]['nama'] = "Acura NSX GT3";
+  $data[5]['deskripsi'] = "1 Desember 2019";
+  $data[5]['tipe'] = "art";
+  $data[5]['link'] = "https://www.instagram.com/p/B5hw4LJgeLF/";
+  $data[5]['foto'] = "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/e35/75207301_2420141644864116_1256531060313996712_n.jpg?_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_cat=100&_nc_ohc=jrJj0mehz0QAX_fK5bk&tp=1&oh=403123544bea29b5b5c489e03b3e8eae&oe=6054DE49&dl=1";
+  
+  $data[6]['nama'] = "Call of Duty Mobile Weapon Score";
+  $data[6]['deskripsi'] = "Skrip ini berfungsi untuk menghitung score dari senjata CODM.";
+  $data[6]['tipe'] = "app";
+  $data[6]['link'] = "https://github.com/tfkhdyt/CODMWeaponScore";
+  $data[6]['foto'] = "assets/img/portfolio/banner-codm-ws.jpg";
+  
+  $data[7]['nama'] = "Nissan 718 Cayman GT4 Clubsport";
+  $data[7]['deskripsi'] = "1 Desember 2019";
+  $data[7]['tipe'] = "art";
+  $data[7]['link'] = "https://www.instagram.com/p/B5hPEqFg7MX/?igshid=1ao9i4cd4go1";
+  $data[7]['foto'] = "https://instagram.fbdo1-2.fna.fbcdn.net/v/t51.2885-15/e35/74638885_186899705799029_8234413037892507777_n.jpg?_nc_ht=instagram.fbdo1-2.fna.fbcdn.net&_nc_cat=105&_nc_ohc=mll5L7Mh_U4AX8DefT4&tp=1&oh=42637ea3f851ac5cb04871a5777906f3&oe=6056FC58";
+  
+  $data[8]['nama'] = "Free Pascal Compiler for Termux";
+  $data[8]['deskripsi'] = "Compiler untuk Free Pascal di Termux";
+  $data[8]['tipe'] = "app";
+  $data[8]['link'] = "https://github.com/tfkhdyt/termux-fpc";
+  $data[8]['foto'] = "assets/img/portfolio/banner-termux-fpc.jpg";
+  
+  $data[9]['nama'] = "Sakura Gakuin 2017 3rd Grade Jojo Effect";
+  $data[9]['deskripsi'] = "21 Juli 2019";
+  $data[9]['tipe'] = "art";
+  $data[9]['link'] = "https://www.instagram.com/p/B0LqAhpg9kh/?igshid=1scbvjxxi85xd";
+  $data[9]['foto'] = "assets/img/portfolio/sg-jojo.jpg";
+
+  $jumlah = 0;
+  foreach ($data as $type) {
+      $jumlah+= count($type);
+  }
+  $jumlah /= 5;
+?>
