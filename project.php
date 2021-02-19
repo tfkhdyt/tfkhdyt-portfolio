8cd7de3d6dd9385818f5226b91b6a54db5b4ddaa
tfkhdyt <ta142fa@gmail.com>
2021-02-19 19:50:48 +0700

    stable release - native js backend

--- project.php
+++ project.php
@@ -2,18 +2,7 @@
   include "data.php";
   for($i=0;$i<$jumlah;$i++):
 ?>
-<div class="col-lg-4 col-md-6 portfolio-item filter-<?=$data[$i]['tipe']?>">
-  <div class="portfolio-wrap">
-    <img src="<?=$data[$i]['foto']?>" class="img-fluid" alt="">
-    <div class="portfolio-info">
-      <h4><?=$data[$i]['nama']?></h4>
-      <p><?=$data[$i]['deskripsi']?></p>
-      <div class="portfolio-links">
-        <a href="<?=$data[$i]['link']?>" target="_blank"><i class="bx bx-window-open"></i></a>
-      </div>
-    </div>
-  </div>
-</div>
+
 <?php
   endfor;
 ?>
