3c2e091523a464dff7c5420d8919e04b803bcab2
tfkhdyt <ta142fa@gmail.com>
2021-05-12 22:14:26 +0700

    nyoba pake js buat ganti metadata

---
+++ keyword.js
@@ -0,0 +1,30 @@
+var keywords = [
+  "taufik hidayat",
+  "Taufik Hidayat",
+  "TAUFIK HIDAYAT",
+  "tfkhdyt",
+  "TFKHDYT",
+  "TFKHDYT Portfolio",
+  "tfkhdyt portfolio",
+  "Taufik Hidayat Portfolio",
+  "taufik hidayat portfolio",
+  "TFKHDYT Web",
+  "tfkhdyt web",
+  "Taufik Hidayat Web",
+  "taufik hidayat web",
+  "TFKHDYT Web Portfolio",
+  "tfkhdyt web portfolio",
+  "Taufik Hidayat Web Portfolio",
+  "taufik hidayat web portfolio",
+  "tafanizer",
+  "tfkhdyt web app"
+];
+//var html = '<meta name="keywords" content="';
+var html = "";
+for(var i = 0; i < keywords.length; i++){
+  var separator = (i < keywords.length - 1) ? ", " : "";
+  html += keywords[i] + separator;
+}
+//tmll += '">';
+//console.log(html);
+document.querySelector('meta[name="keywords"]').setAttribute("content", html);
