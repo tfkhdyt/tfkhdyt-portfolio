494f55a2bcd1a24dadb77a37ef7171122b404fb0
tfkhdyt <ta142fa@gmail.com>
2021-06-16 06:14:14 +0700

    Revert "coba ganti di js"
    This reverts commit b1f492a2c65694a29154c6c8583fd4b6af1f416b.

--- assets/js/main.js
+++ assets/js/main.js
@@ -248,10 +248,10 @@
 
 // Back to top button
 $(window).scroll(function() {
-  if ($(this).scrollTop() > 150) {
-    $('.back-to-top').fadeIn(500);
+  if ($(this).scrollTop() > 100) {
+    $('.back-to-top').fadeIn('slow');
   } else {
-    $('.back-to-top').fadeOut(500);
+    $('.back-to-top').fadeOut('slow');
   }
 });
 
