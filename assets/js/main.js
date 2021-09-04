37f75a5df06f3b930552149d3c0169c1aa57fd5e
tfkhdyt <ta142fa@gmail.com>
2021-09-04 20:31:15 +0700

    back ke sebelum bikin loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -290,22 +290,16 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbz4zeawNe9YCQSl4nbDV_RmtmxI5mC38UlXGbGBIq3FcLTm2AgRpcpJnZGOqiLbwiDB/exec';
 const form = document.forms['submit-to-google-sheet'];
 const success = document.querySelector('.sent-message');
-const loadingContact = document.querySelector('.loadingContact');
-loadingContact.style.display = 'none';
 
 form.addEventListener('submit', e => {
   e.preventDefault();
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => {
-      loadingContact.style.display = 'inline-block';
-      if (response.status == 200) {
-        console.log('Success!', response)
-        loadingContact.style.display = 'none';
-        success.style.display = 'inline-block';
-        setTimeout(() => {
-          success.style.display = 'none';
-        }, 5000);
-      }
+      console.log('Success!', response)
+      success.style.display = 'block';
+      setTimeout(() => {
+        success.style.display = 'none';
+      }, 5000);
     })
     .catch(error => {
       console.error('Error!', error.message)
